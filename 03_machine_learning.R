d28f8f01eabcfb42a4a1294944ed9777d0e407e1
Jason Dude <dudej@wustl.edu>
2026-01-16 23:41:59 -0700

    upd

--- 03_machine_learning.R
+++ 03_machine_learning.R
@@ -1,6 +1,27 @@
+library(dplyr)
 library(tidymodels)
 
-df_train <- readRDS("df_train.RDS")
+df_features <- read.csv("data/features/actigraphy_features.csv")
+df_features <- df_features |> mutate(worn = as.factor(case_when(worn == TRUE ~ 1, worn == FALSE ~ 0)))
+split_obj <- group_initial_split(
+  df_features,
+  group = id,
+  prop = 0.7
+)
+train_df  <- training(split_obj)
+test_df   <- testing(split_obj)
+
+train_df |> 
+  group_by(worn) |> 
+  summarise(count = n()) |> 
+  ungroup() |> 
+  mutate(prop = count / sum(count))
+
+test_df |> 
+  group_by(worn) |> 
+  summarise(count = n()) |> 
+  ungroup() |> 
+  mutate(prop = count / sum(count))
 
 non_predictors <- c("date_time", "id", "index", "button_press_time_sum")
 
