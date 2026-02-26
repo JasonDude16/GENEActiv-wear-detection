b35a215bd4aab467ed22743e43ace483bc55d71e
Jason Dude <dudej@wustl.edu>
2026-02-26 10:17:30 -0700

    add comments

--- src/supp_bootstrapped_estimates.R
+++ src/supp_bootstrapped_estimates.R
@@ -2,11 +2,10 @@ library(dplyr)
 library(purrr)
 library(tidymodels)
 
+# helper for fitting and evaluating model once 
 fit_eval_once_oob <- function(df, boot_ids, threshold = 0.55) {
   
-  w_tbl <- tibble(id = boot_ids) |>
-    count(id, name = "w")
-  
+  w_tbl <- tibble(id = boot_ids) |> count(id, name = "w")
   inbag_ids <- w_tbl$id
   oob_ids   <- setdiff(unique(df$id), inbag_ids)
   if (length(oob_ids) < 1) return(NULL)
@@ -16,8 +15,7 @@ fit_eval_once_oob <- function(df, boot_ids, threshold = 0.55) {
     left_join(w_tbl, by = "id") |>
     mutate(w = hardhat::frequency_weights(w))
   
-  test_df <- df |>
-    filter(id %in% oob_ids)
+  test_df <- df |> filter(id %in% oob_ids)
   
   non_predictors <- c(
     "date_time",
@@ -56,7 +54,7 @@ fit_eval_once_oob <- function(df, boot_ids, threshold = 0.55) {
   
   xgb_fit <- fit(wf, data = train_df)
   
-  # robustly grab the event prob (yardstick event_level="second")
+  # grab the event prob (yardstick event_level = "second")
   p <- predict(xgb_fit, test_df, type = "prob")
   event_class <- levels(test_df$label_is_worn)[2]
   probs <- p[[paste0(".pred_", event_class)]]
@@ -68,16 +66,21 @@ fit_eval_once_oob <- function(df, boot_ids, threshold = 0.55) {
       .pred = factor(ifelse(.prob >= threshold, lvl[2], lvl[1]), levels = lvl)
     )
   
+  # metrics/summary
   tibble(
     bal_acc = bal_accuracy(test_out, truth = label_is_worn, estimate = .pred, event_level = "second")$.estimate,
-    auc     = roc_auc(test_out, truth = label_is_worn, .prob, event_level = "second")$.estimate,
+    auc = roc_auc(test_out, truth = label_is_worn, .prob, event_level = "second")$.estimate,
     n_inbag_ids = length(inbag_ids),
-    n_oob_ids   = length(oob_ids)
+    n_oob_ids = length(oob_ids)
   )
 }
 
+# -------------------------------------------------------------------------------------------------------------------------------------
+
+# get list of ids for sampling
 ids <- unique(df_modeling$id)
 
+# model N times with randomly sampled ids and save results
 set.seed(1)
 boot_results <- map_dfr(seq_len(50), function(i) {
   tictoc::tic()
@@ -100,6 +103,7 @@ boot_summary <- boot_results |>
   ) 
 print(boot_summary)
 
+# balanced accuracy distribution
 hist(
   boot_results$bal_acc,
   breaks = 15,
@@ -110,6 +114,7 @@ abline(v = median(boot_results$bal_acc), lty = 2, lwd = 2)
 abline(v = boot_summary$bal_acc_mean + 2*(boot_summary$bal_acc_sd), lty = 2, lwd = 1, col = "red")
 abline(v = boot_summary$bal_acc_mean - 2*(boot_summary$bal_acc_sd), lty = 2, lwd = 1, col = "red")
 
+# AUC distribution
 hist(
   boot_results$auc,
   breaks = 15,
